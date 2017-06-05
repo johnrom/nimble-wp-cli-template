33b63a3f6171374081c7047c41c4f60ee755488e
John Rom <web@johnrom.com>
2017-06-05 18:51:27 -0400

    Added support for Behat Feature and PHP Unit Testing

--- hooks/test.sh
+++ hooks/test.sh
@@ -1,2 +1,12 @@
-# test behat
-nimble bash "$project" "XDEBUG_CONFIG='' ./vendor/bin/behat --expand"
+
+# test behat or phpunit
+echo "${*:5}"
+
+case "$4" in
+    behat)
+        nimble bash "$project" "XDEBUG_CONFIG='' ./vendor/bin/behat --expand ${*:5}"
+        ;;
+    phpunit)
+        nimble bash "$project" "XDEBUG_CONFIG='' ./vendor/bin/phpunit ${*:5}"
+        ;;
+esac
